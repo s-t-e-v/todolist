8797bfe84da8d8493964e2a6094c9e5118c19cb8
s-t-e-v <steven.bandaogo@proton.me>
2023-01-04 13:37:29 +0100

    connect index and deletion views

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    taskname = models.CharField(max_length=255)
+    checkstate = models.BooleanField(default=False)
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,34 @@
 from django.template import loader
 from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
 
+## index.html views
 def index(request):
+    todo = Todo.objects.all().values()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    context = {
+        'todo': todo,
+    }
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+    
+    pass
+
+def check(request):
+    pass
+
+def check_update(request):
+    pass
+
+def deletion(request):
+    todo = Todo.objects.all().values()
+    template = loader.get_template('deletion.html')
+    context = {
+        'todo': todo,
+    }
+    return HttpResponse(template.render(context, request))
+
+## deletion.html views
