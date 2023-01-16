16a7bae9038615e2d19911d2cd72bdf1e70492b4
s-t-e-v <steven.bandaogo@proton.me>
2023-01-16 18:57:22 +0100

    comment js file

--- todo/static/todo/script.js
+++ todo/static/todo/script.js
@@ -2,8 +2,9 @@ async function add_request() {
     // We retrieve the value in the entry bar
     let taskname = document.getElementById("taskentry").value;
 
+    // if there is nothing inside the text bar
     if (taskname == "") {
-        return 0;
+        return 0;// we do nothing
     }
 
     console.log(taskname);
@@ -14,17 +15,20 @@ async function add_request() {
     try {
         console.log('we are inside!');
         // Make a POST request to the Django view to create the task
-        const formData = new FormData();
+        const formData = new FormData(); // datastructure needed for the python code in views.py
         formData.append('taskname', taskname);
+
+        // request
         const response = await fetch("add/", {
             method: "POST",
             body: formData,
             headers: {
                 'X-CSRFToken': getCookie('csrftoken')
             },
-        credentials: 'same-origin',
+        credentials: 'same-origin', // ?
         });
 
+        // response
         const data = await response.json();
         console.log(data);
 
@@ -34,9 +38,10 @@ async function add_request() {
     }
 }
 
+// retrieve the cookie to give it to Django server (because of CSRFToken activated)
 function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
-  }
+}
   
